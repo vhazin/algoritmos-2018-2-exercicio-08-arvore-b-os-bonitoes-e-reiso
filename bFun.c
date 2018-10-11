bace23d2980f196a16abe4a012079d1347eddb78
OtacilioN <otacilio.n97@gmail.com>
2018-10-11 08:03:09 -0300

    Fix compilation warnings and add main.c flow

--- bFun.c
+++ bFun.c
@@ -2,11 +2,6 @@
 #include <stdlib.h>
 #include "bFun.h"
 
-#define M 3
-#define GMIN 2 // grau minimo da arvore (se precisar, fazer por metodo de input)
-
-// tecnicamente funções que serão usadas no insert estão prontas
-
 void create(bTree *Tree, int order) // recebe ponteiro para arvore
 {
     node *new = (node *)malloc(sizeof(node)); //cria node
@@ -16,53 +11,52 @@ void create(bTree *Tree, int order) // recebe ponteiro para arvore
     Tree->order = order;
 }
 
-
-void splitChild(node *father, int position) // função para dar split
+void splitChild(node *father, int position, int order) // função para dar split
 {
     int count;
+    int t = (order - 1) / 2;
     node *new = (node *)malloc(sizeof(node));
     node *child = father->childs[position];
     new->isLeave = child->isLeave;
-    new->nKeys = GMIN - 1;
-    for (count = 0; count < (GMIN - 1); count++)
-        new->keys[count] = child->keys[count + GMIN];
+    new->nKeys = t;
+    for (count = 0; count < (t); count++)
+        new->keys[count] = child->keys[count + t + 1];
     if (!child->isLeave)
     {
-        for (count = 0; count < (GMIN-1); count++)
-            new->childs[count] = child->childs[count+GMIN];
+        for (count = 0; count < (t); count++)
+            new->childs[count] = child->childs[count + t + 1];
     }
-    child->nKeys = GMIN - 1;
-    for (count = father->nKeys+1; count > (position-1); count--)
-        father->keys[count+1] = father->keys[count];
-    father->keys[position] = child->keys[GMIN];
+    child->nKeys = t;
+    for (count = father->nKeys + 1; count > (position - 1); count--)
+        father->keys[count + 1] = father->keys[count];
+    father->keys[position] = child->keys[t + 1];
     father->nKeys += 1;
 }
-
 
 void insert(bTree *myTree, int value) // função para inserir novo nó
 {
     node *Root = myTree->root;
-
-    if (Root->nKeys)
+    int t = (myTree->order - 1) / 2;
+    if (Root->nKeys >= 2 * t - 1)
     {
         node *newroot = (node *)malloc(sizeof(node));
         myTree->root = newroot;
         newroot->isLeave = FALSE;
         newroot->nKeys = 0;
-        newroot->keys[0] = Root;
-        splitChild(newroot, 1);
-        insertNonfull(s, value);
+        newroot->keys[0] = Root->keys[0];
+        splitChild(newroot, 1, myTree->order);
+        insertNonfull(newroot, value, myTree->order);
     }
     else
     {
-        insertNonfull(Root, value);
+        insertNonfull(Root, value, myTree->order);
     }
 }
 
-
-int insertNonfull(node *Node, int value) // inserir se ja tiver outros valores no nó
+void insertNonfull(node *Node, int value, int order) // inserir se ja tiver outros valores no nó
 {
     int numkey = Node->nKeys;
+    int t = (order - 1) / 2;
 
     if (Node->isLeave)
     {
@@ -79,32 +73,26 @@ int insertNonfull(node *Node, int value) // inserir se ja tiver outros valores n
         while (numkey >= Node->nKeys && value < Node->keys[numkey])
             numkey -= 1;
         numkey += 1;
-        if (Node->childs[numkey]->nKeys == (2 * GMIN) - 1)
+        if (Node->childs[numkey]->nKeys == (2 * t) - 1)
         {
-            splitChild(Node, numkey);
+            splitChild(Node, numkey, order);
             if (value > Node->keys[numkey])
                 numkey += 1;
         }
-        insertNonfull(Node->childs[numkey], value);
+        insertNonfull(Node->childs[numkey], value, order);
     }
 }
 
-
-void printar(bTree *raiz)
+void print(node *root)
 {
     int i;
-    if (raiz->root != NULL)
+    if (root != NULL)
     {
-        for (i = 0; i < raiz->root->nKeys; i++)
+        for (i = 0; i < root->nKeys; i++)
         {
-            printar(raiz->root->childs[i]);
-            printf("\n%d", raiz->root->keys[i]);
+            print(root->childs[i]);
+            printf("\n%d", root->keys[i]);
         }
-        printar(raiz->root->childs[i]);
+        print(root->childs[i]);
     }
-}
-
-
-int main(void) {
-
 }
