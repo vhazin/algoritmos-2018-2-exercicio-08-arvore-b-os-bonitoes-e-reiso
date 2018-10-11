21e11294ba1e9927fc95ac3ec01c722eb015aca0
Arthur O. Reis <aor@cesar.school>
2018-10-10 22:05:11 -0300

    só precisa de main para testar as funções

--- bFun.c
+++ bFun.c
@@ -3,6 +3,9 @@
 #include "bFun.h"
 
 #define M 3
+#define GMIN 2 // grau minimo da arvore (se precisar, fazer por metodo de input)
+
+// tecnicamente funções que serão usadas no insert estão prontas
 
 void create(bTree *Tree, int order) // recebe ponteiro para arvore
 {
@@ -13,17 +16,30 @@ void create(bTree *Tree, int order) // recebe ponteiro para arvore
     Tree->order = order;
 }
 
-void splitChild(node *father, int position, int order)
+
+void splitChild(node *father, int position) // função para dar split
 {
-    int j, t = (order - 1) / 2;
+    int count;
     node *new = (node *)malloc(sizeof(node));
     node *child = father->childs[position];
     new->isLeave = child->isLeave;
-    new->nKeys = t;
-    for (j = 1; j < t; j++)
+    new->nKeys = GMIN - 1;
+    for (count = 0; count < (GMIN - 1); count++)
+        new->keys[count] = child->keys[count + GMIN];
+    if (!child->isLeave)
+    {
+        for (count = 0; count < (GMIN-1); count++)
+            new->childs[count] = child->childs[count+GMIN];
+    }
+    child->nKeys = GMIN - 1;
+    for (count = father->nKeys+1; count > (position-1); count--)
+        father->keys[count+1] = father->keys[count];
+    father->keys[position] = child->keys[GMIN];
+    father->nKeys += 1;
 }
 
-void insert(bTree *myTree, int value)
+
+void insert(bTree *myTree, int value) // função para inserir novo nó
 {
     node *Root = myTree->root;
 
@@ -43,30 +59,52 @@ void insert(bTree *myTree, int value)
     }
 }
 
-int insertNonfull(node *Node, int value)
+
+int insertNonfull(node *Node, int value) // inserir se ja tiver outros valores no nó
 {
-    int i = Node->nKeys;
+    int numkey = Node->nKeys;
 
     if (Node->isLeave)
     {
-        while (i >= 0 && value < Node->keys[i])
+        while (numkey >= 0 && value < Node->keys[numkey])
         {
-            Node->keys[i + 1] = Node->keys[i];
-            i--;
+            Node->keys[numkey + 1] = Node->keys[numkey];
+            numkey -= 1;
         }
-        Node->keys[i + 1] = value;
+        Node->keys[numkey + 1] = value;
         Node->nKeys = Node->nKeys + 1;
     }
     else
     {
-        while (i >= Node->nKeys && value < Node->keys[i])
-        { // descobrir condicional de i
-            i--;
+        while (numkey >= Node->nKeys && value < Node->keys[numkey])
+            numkey -= 1;
+        numkey += 1;
+        if (Node->childs[numkey]->nKeys == (2 * GMIN) - 1)
+        {
+            splitChild(Node, numkey);
+            if (value > Node->keys[numkey])
+                numkey += 1;
         }
-        i++;
-        if (/* condition */)
-        { // completar
-            /* code */
-        }
+        insertNonfull(Node->childs[numkey], value);
     }
+}
+
+
+void printar(bTree *raiz)
+{
+    int i;
+    if (raiz->root != NULL)
+    {
+        for (i = 0; i < raiz->root->nKeys; i++)
+        {
+            printar(raiz->root->childs[i]);
+            printf("\n%d", raiz->root->keys[i]);
+        }
+        printar(raiz->root->childs[i]);
+    }
+}
+
+
+int main(void) {
+
 }
